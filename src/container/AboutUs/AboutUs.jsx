b4e5f5454debe264599d56fe0b67450263cdbd98
Richard Kim <Richard.kim.0000@gmail.com>
2022-02-10 17:19:03 -0800

    Finished Header section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import './AboutUs.css';
+import { images } from "../../constants";
 
-const AboutUs = () => (
-  <div>
-    AboutUs
-  </div>
-);
+const AboutUs = () => {
+  return (
+    <div
+      className="app_aboutus app__bg flex__center section__padding"
+      id="about"
+    >
+      <div className="app__aboutus-overlay flex__center">
+        <img src={images.G} alt="G letter" />
+      </div>
+      AboutUs
+    </div>
+  );
+};
 
 export default AboutUs;
